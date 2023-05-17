8918f72cb3c2e8d0650127fa947efeafe1a08b68
sillyfish <sillyfish87@gmail.com>
2023-05-17 16:56:57 +0800

    Alien invasion init before creating a settings class

---
+++ python_crash_course/alien_invasion/settings.py
@@ -0,0 +1,21 @@
+"""
+Title: 
+Date: 2023-05-17 16:53:46
+LastEditTime: 2023-05-17 16:53:46
+Category: 
+Tags: 
+Slug: 
+Authors: 惊人不易
+Summary: 
+"""
+
+
+class Settings:
+    """存储《外星人入侵》的所有设置的类"""
+
+    def __init__(self):
+        """初始化游戏的设置"""
+        # 屏幕设置
+        self.screen_width = 1200
+        self.screen_height = 800
+        self.bg_color = (230, 230, 230)
