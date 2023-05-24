f5ee4c4ae32532a70a25e70c7424cfc316526365
sillyfish <sillyfish87@gmail.com>
2023-05-24 15:59:05 +0800

    [Alien invasion]Q 2 quit, and fullscreen mode.

--- python_crash_course/alien_invasion/alien_invasion.py
+++ python_crash_course/alien_invasion/alien_invasion.py
@@ -28,9 +28,10 @@ class AlienInvasion:
         self.clock = pygame.time.Clock()
         self.settings = Settings()
 
-        self.screen = pygame.display.set_mode(
-            (self.settings.screen_width, self.settings.screen_height)
-        )
+        self.screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
+        self.settings.screen_width = self.screen.get_rect().width
+        self.settings.screen_height = self.screen.get_rect().height
+
         pygame.display.set_caption("Alien Invasion")
 
         self.ship = Ship(self)
@@ -75,6 +76,8 @@ class AlienInvasion:
             self.ship.moving_right = True
         elif event.key == pygame.K_LEFT:
             self.ship.moving_left = True
+        elif event.key == pygame.K_q:
+            sys.exit()
 
 
 if __name__ == "__main__":
