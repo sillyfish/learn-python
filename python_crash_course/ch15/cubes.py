40c01e0031b05de072a1daaa6bef1c53db7d8cbb
sillyfish <sillyfish87@gmail.com>
2023-06-07 18:08:35 +0800

    [ch15]Cubes and colored cubes.

---
+++ python_crash_course/ch15/cubes.py
@@ -0,0 +1,9 @@
+import matplotlib.pyplot as plt
+
+x_values = range(1, 5001)
+y_values = [x**3 for x in x_values]
+
+fig, ax = plt.subplots()
+ax.scatter(x_values, y_values, c=y_values, cmap=plt.cm.Reds, s=10)
+
+plt.show()
