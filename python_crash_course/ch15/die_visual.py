69180d997184cee0a738c90668d3bd98840ec909
sillyfish <sillyfish87@gmail.com>
2023-07-24 16:59:08 +0800

    [ch15]15-7. Three Dice.

--- python_crash_course/ch15/die_visual.py
+++ python_crash_course/ch15/die_visual.py
@@ -2,25 +2,26 @@ import plotly.express as px
 from die import Die
 
 # Create a D6.
-die_1 = Die(8)
-die_2 = Die(8)
+die_1 = Die()
+die_2 = Die()
+die_3 = Die()
 
 # Make some rolls, and store results in a list.
 results = []
-for roll_num in range(1_000):
-    result = die_1.roll() + die_2.roll()
+for roll_num in range(50_000):
+    result = die_1.roll() + die_2.roll() + die_3.roll()
     results.append(result)
 
 # Analyze the results.
 frequencies = []
-max_result = die_1.num_sides + die_2.num_sides
-poss_results = range(2, max_result + 1)
+max_result = die_1.num_sides + die_2.num_sides + die_3.num_sides
+poss_results = range(3, max_result + 1)
 for value in poss_results:
     frequency = results.count(value)
     frequencies.append(frequency)
 
 # Visualize the results.
-title = "Results of Rolling Two D8 1,000 Times."
+title = "Results of Rolling Three D6 50,000 Times."
 label = {"x": "Result", "y": "Frequency of Result"}
 fig = px.bar(x=poss_results, y=frequencies, labels=label, title=title)
 
