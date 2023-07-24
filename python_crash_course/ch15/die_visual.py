ce9ad4d20ed8893c1bb375f5a0660746e992a8b5
sillyfish <sillyfish87@gmail.com>
2023-07-24 16:56:55 +0800

    [ch15]15-6. Two D8s.

--- python_crash_course/ch15/die_visual.py
+++ python_crash_course/ch15/die_visual.py
@@ -2,12 +2,12 @@ import plotly.express as px
 from die import Die
 
 # Create a D6.
-die_1 = Die()
-die_2 = Die(10)
+die_1 = Die(8)
+die_2 = Die(8)
 
 # Make some rolls, and store results in a list.
 results = []
-for roll_num in range(50_000):
+for roll_num in range(1_000):
     result = die_1.roll() + die_2.roll()
     results.append(result)
 
@@ -20,7 +20,7 @@ for value in poss_results:
     frequencies.append(frequency)
 
 # Visualize the results.
-title = "Results of Rolling Two D10 50,000 Times."
+title = "Results of Rolling Two D8 1,000 Times."
 label = {"x": "Result", "y": "Frequency of Result"}
 fig = px.bar(x=poss_results, y=frequencies, labels=label, title=title)
 
