2dc7578e593171d37741799982ca67bbaad322f5
sillyfish <sillyfish87@gmail.com>
2023-07-24 17:11:57 +0800

    [ch15]15-9. Die Comprehensions.

--- python_crash_course/ch15/die_visual.py
+++ python_crash_course/ch15/die_visual.py
@@ -6,18 +6,12 @@ die_1 = Die()
 die_2 = Die()
 
 # Make some rolls, and store results in a list.
-results = []
-for roll_num in range(50_000):
-    result = die_1.roll() * die_2.roll()
-    results.append(result)
+results = [die_1.roll() * die_2.roll() for roll_num in range(50_000)]
 
 # Analyze the results.
-frequencies = []
 max_result = die_1.num_sides * die_2.num_sides
 poss_results = range(1, max_result + 1)
-for value in poss_results:
-    frequency = results.count(value)
-    frequencies.append(frequency)
+frequencies = [results.count(value) for value in poss_results]
 
 # Visualize the results.
 title = "Results of Rolling Two D6 50,000 Times."
