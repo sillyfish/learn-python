2732f0599a10c0e946d42292c8e24be7ae2e9a43
sillyfish <sillyfish87@gmail.com>
2023-07-24 17:01:09 +0800

    [ch15]15-8. Multiplication.

--- python_crash_course/ch15/die_visual.py
+++ python_crash_course/ch15/die_visual.py
@@ -4,24 +4,23 @@ from die import Die
 # Create a D6.
 die_1 = Die()
 die_2 = Die()
-die_3 = Die()
 
 # Make some rolls, and store results in a list.
 results = []
 for roll_num in range(50_000):
-    result = die_1.roll() + die_2.roll() + die_3.roll()
+    result = die_1.roll() * die_2.roll()
     results.append(result)
 
 # Analyze the results.
 frequencies = []
-max_result = die_1.num_sides + die_2.num_sides + die_3.num_sides
-poss_results = range(3, max_result + 1)
+max_result = die_1.num_sides * die_2.num_sides
+poss_results = range(1, max_result + 1)
 for value in poss_results:
     frequency = results.count(value)
     frequencies.append(frequency)
 
 # Visualize the results.
-title = "Results of Rolling Three D6 50,000 Times."
+title = "Results of Rolling Two D6 50,000 Times."
 label = {"x": "Result", "y": "Frequency of Result"}
 fig = px.bar(x=poss_results, y=frequencies, labels=label, title=title)
 
