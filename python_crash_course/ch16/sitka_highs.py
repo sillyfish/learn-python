00166c0907d4e6c77fd9073e352f9ea353015f10
sillyfish <sillyfish87@gmail.com>
2023-07-24 17:42:56 +0800

    [ch16]Plotting data in a temperature chart.

---
+++ python_crash_course/ch16/sitka_highs.py
@@ -0,0 +1,33 @@
+import csv
+from pathlib import Path
+from datetime import datetime
+
+import matplotlib.pyplot as plt
+
+path = Path("weather_data/sitka_weather_07-2021_simple.csv")
+lines = path.read_text().splitlines()
+
+reader = csv.reader(lines)
+header_row = next(reader)
+
+# Extract dates and high temperatures.
+dates, highs = [], []
+for row in reader:
+    current_date = datetime.strptime(row[2], "%Y-%m-%d")
+    high = int(row[4])
+    dates.append(current_date)
+    highs.append(high)
+
+# Plot the high temperature.
+plt.style.use("seaborn")
+fig, ax = plt.subplots()
+ax.plot(dates, highs, color="red")
+
+# Format plot.
+ax.set_title("Daily high temperatures, July 2021", fontsize=24)
+ax.set_xlabel("", fontsize=16)
+fig.autofmt_xdate()
+ax.set_ylabel("Temperature (F)", fontsize=16)
+ax.tick_params(labelsize=16)
+
+plt.show()
