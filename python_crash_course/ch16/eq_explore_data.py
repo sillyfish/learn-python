6c4db8e03ba9f3fdd50df13414171b19618c52ac
sillyfish <sillyfish87@gmail.com>
2023-07-26 15:12:47 +0800

    [ch16]16-8. Recent Earthquakes.

--- python_crash_course/ch16/eq_explore_data.py
+++ python_crash_course/ch16/eq_explore_data.py
@@ -4,7 +4,7 @@ import json
 import plotly.express as px
 
 # Read data as a string and convert to a Python object.
-path = Path("eq_data/eq_data_30_day_m1.geojson")
+path = Path("eq_data/eq_data_30_day_m2.geojson")
 contents = path.read_text()
 all_eq_data = json.loads(contents)
 
@@ -13,10 +13,12 @@ all_eq_dicts = all_eq_data["features"]
 
 mags, lons, lats, eq_titles = [], [], [], []
 for eq_dict in all_eq_dicts:
-    mags.append(eq_dict["properties"]["mag"])
-    lons.append(eq_dict["geometry"]["coordinates"][0])
-    lats.append(eq_dict["geometry"]["coordinates"][1])
-    eq_titles.append(eq_dict["properties"]["title"])
+    mag = eq_dict["properties"]["mag"]
+    if mag > 0:
+        mags.append(mag)
+        lons.append(eq_dict["geometry"]["coordinates"][0])
+        lats.append(eq_dict["geometry"]["coordinates"][1])
+        eq_titles.append(eq_dict["properties"]["title"])
 
 title = all_eq_data["metadata"]["title"]
 fig = px.scatter_geo(
