5d648df93063798459a546e63e49fbaf24dc9082
sillyfish <sillyfish87@gmail.com>
2023-07-26 15:00:21 +0800

    [ch16]16-6. Refactoring.

--- python_crash_course/ch16/eq_explore_data.py
+++ python_crash_course/ch16/eq_explore_data.py
@@ -13,14 +13,10 @@ all_eq_dicts = all_eq_data["features"]
 
 mags, lons, lats, eq_titles = [], [], [], []
 for eq_dict in all_eq_dicts:
-    mag = eq_dict["properties"]["mag"]
-    lon = eq_dict["geometry"]["coordinates"][0]
-    lat = eq_dict["geometry"]["coordinates"][1]
-    eq_title = eq_dict["properties"]["title"]
-    mags.append(mag)
-    lons.append(lon)
-    lats.append(lat)
-    eq_titles.append(eq_title)
+    mags.append(eq_dict["properties"]["mag"])
+    lons.append(eq_dict["geometry"]["coordinates"][0])
+    lats.append(eq_dict["geometry"]["coordinates"][1])
+    eq_titles.append(eq_dict["properties"]["title"])
 
 title = "Global Earthquakes"
 fig = px.scatter_geo(
