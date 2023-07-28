10ae0f1108dd53f596995635cb53229dcb092632
sillyfish <sillyfish87@gmail.com>
2023-07-28 17:02:17 +0800

    [ch17]17-2. Active Discussions.

--- python_crash_course/ch17/hn_submissions.py
+++ python_crash_course/ch17/hn_submissions.py
@@ -1,4 +1,4 @@
-from operator import itemgetter
+import plotly.express as px
 
 import requests
 
@@ -9,7 +9,7 @@ print(f"Status code: {r.status_code}")
 
 # Process information about each submission.
 submission_ids = r.json()
-submission_dicts = []
+links, comments = [], []
 for submission_id in submission_ids[:5]:
     # Make a new API call for each submission.
     url = f"https://hacker-news.firebaseio.com/v0/item/{submission_id}.json"
@@ -18,16 +18,12 @@ for submission_id in submission_ids[:5]:
     response_dict = r.json()
 
     # Build a dictionary for each article.
-    submission_dict = {
-        "title": response_dict["title"],
-        "hn_link": f"https://news.ycombinator.com/item?id={submission_id}",
-        "comments": response_dict["descendants"],
-    }
-    submission_dicts.append(submission_dict)
+    hn_link = f"https://news.ycombinator.com/item?id={submission_id}"
+    title = response_dict["title"]
+    link = f"<a href='{hn_link}'>{title}</a>"
+    links.append(link)
+    comments.append(response_dict["descendants"])
 
-submission_dicts = sorted(submission_dicts, key=itemgetter("comments"), reverse=True)
-
-for submission_dict in submission_dicts:
-    print(f"\nTitle: {submission_dict['title']}")
-    print(f"Discussion link: {submission_dict['hn_link']}")
-    print(f"Comments: {submission_dict['comments']}")
+labels = {"x": "Article", "y": "Comments"}
+fig = px.bar(x=links, y=comments, title="Most-Commented Articles on Hacker News")
+fig.show()
