90b1c9f1e2fd9c2cea5f905d97ff297873e51601
sillyfish <sillyfish87@gmail.com>
2023-07-28 16:49:29 +0800

    [ch17]17-1. Other Languages.

--- python_crash_course/ch17/python_repos_visual.py
+++ python_crash_course/ch17/python_repos_visual.py
@@ -3,7 +3,7 @@ import plotly.express as px
 
 # Make an API call and check the response.
 url = "https://api.github.com/search/repositories"
-url += "?q=language:python+sort:stars+stars:>10000"
+url += "?q=language:go+sort:stars+stars:>10000"
 
 headers = {"Accept": "application/vnd.github.v3+json"}
 r = requests.get(url, headers=headers)
